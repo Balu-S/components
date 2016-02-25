68a09c4b2d6ac05977020229dda438c26a5092ab
SowmyaT <tsowmya512@gmail.com>
2016-02-25 12:03:47 +0530

    $ symbol removed

--- app/components/menu-component.js
+++ app/components/menu-component.js
@@ -3,24 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   subMenuObj : [],
-
+  isShowSubMenu: false,
 
   mouseMove( event ){
     event.stopPropagation();
     var subMenus = this.get('menuItem.children');
     this.set('subMenuObj', subMenus);
-    if(subMenus.length)
-    {
-      $(".ui.dropdown.item").addClass("active visible");
-      $(".ui.dropdown.item .menu.transition" ).removeClass("hidden");
-      $(".ui.dropdown.item .menu.transition").addClass("visible");
-      $('#submenudiv').css({'position': 'absolute', 'display': 'block'});
+    console.log(this.get('isShowSubMenu'));
+    if(subMenus.length){
+      this.set('isShowSubMenu', true);
     }
     else {
-      $( ".ui.dropdown.item").removeClass("active visible");
-      $( ".ui.dropdown.item .menu.transition" ).removeClass("visible");
-      $(".ui.dropdown.item .menu.transition" ).addClass("hidden");
-      $('#submenudiv').css({'display': 'none'});
+      this.set('isShowSubMenu', false);
     }
   }
 });
