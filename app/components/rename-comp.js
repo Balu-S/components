985ab7c3f41b550adbe317037ebf00004673ff25
Renukutty <renugadevi.p@zohocorp.com>
2016-02-25 10:41:04 +0530

    unwanted variables are removed, notes added

--- app/components/rename-comp.js
+++ app/components/rename-comp.js
@@ -7,38 +7,39 @@ export default Ember.Component.extend({
     this.$().focus();
   },
 
-  keyDown: function (event,old_name) {
+  keyDown: function (event) {
 
     var new_name = this.get('renameList.res_name');
 
 
     if(new_name.length < 1){
       alert('Name is Empty, Please Type Something');
-      this.$().focus();
+      // this.$('input#renameInp').focus();
     }
 
-      if ( event.keyCode === 13) {
+      if ( event.keyCode === 13) { // NOTE: Enter key action
           this.get('renameList').set('is_rename', false);
           this.get('renameList').set('res_name', new_name);
           console.log(this.get('renameList'));
       }
 
-      if ( event.keyCode === 27 ) {
-        // this.get('renameList').set('res_name', old_name);
+      if ( event.keyCode === 27 ) {// NOTE: Esc key action
+
         this.get('renameList').set('is_rename', false);
+
       }
 
 
   },
 
-  focusOut:function (new_name) { // NOTE: when focusOut inputbox hide
-      // console.log("Foucus Out");
+  focusOut:function () { // NOTE: when focusOut inputbox hide
+
       this.get('renameList').set('is_rename', false);
 
   },
 
   actions: {
-    showName: function(name) { // NOTE: to change input box
+    showName: function() { // NOTE: to change input box
       this.get('renameList').set('is_rename', true);
     }
   }
