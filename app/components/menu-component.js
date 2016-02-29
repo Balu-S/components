b092c963f0a6d32cc56518a1f61b9595610dc025
Mohamed-Ali-SMA <mohamedali.s@zohocorp.com>
2016-02-29 11:13:38 +0530

    Revert "$ symbol removed"
    This reverts commit 68a09c4b2d6ac05977020229dda438c26a5092ab.

--- app/components/menu-component.js
+++ app/components/menu-component.js
@@ -3,18 +3,24 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   subMenuObj : [],
-  isShowSubMenu: false,
+
 
   mouseMove( event ){
     event.stopPropagation();
     var subMenus = this.get('menuItem.children');
     this.set('subMenuObj', subMenus);
-    console.log(this.get('isShowSubMenu'));
-    if(subMenus.length){
-      this.set('isShowSubMenu', true);
+    if(subMenus.length)
+    {
+      $(".ui.dropdown.item").addClass("active visible");
+      $(".ui.dropdown.item .menu.transition" ).removeClass("hidden");
+      $(".ui.dropdown.item .menu.transition").addClass("visible");
+      $('#submenudiv').css({'position': 'absolute', 'display': 'block'});
     }
     else {
-      this.set('isShowSubMenu', false);
+      $( ".ui.dropdown.item").removeClass("active visible");
+      $( ".ui.dropdown.item .menu.transition" ).removeClass("visible");
+      $(".ui.dropdown.item .menu.transition" ).addClass("hidden");
+      $('#submenudiv').css({'display': 'none'});
     }
   }
 });
